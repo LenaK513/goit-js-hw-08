dd9f5437cb30594d5c5f91174b2bf2a5c1bf281c
Olena <kryvtsovaolena@gmail.com>
2022-08-27 01:05:33 +0300

    add currenttime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function (currentTime) {
+  console.log('played the video!', currentTime);
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
