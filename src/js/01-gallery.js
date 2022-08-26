4564225f34337337c0a6e87da23fa2302c2975e4
Olena <kryvtsovaolena@gmail.com>
2022-08-26 23:00:42 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,10 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const listOfElements = galleryItem => {
   return `
 <div class="gallery__item">
