f99f728c149d934e5a3ef9a0316f9e785385430d
Olena <kryvtsovaolena@gmail.com>
2022-08-26 22:58:42 +0300

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,69 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import apiServise from './export';
+const listOfElements = galleryItem => {
+  return `
+<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+   src="${galleryItem.preview}"
+    data-source="${galleryItem.original}"
+    alt='${galleryItem.description}'
+  />
+</a>
+</div>
+`;
+};
+const listOfImages = galleryItems.map(listOfElements).join('');
 
-console.log(galleryItems);
-console.log(apiServise);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML('beforeend', listOfImages);
+
+gallery.addEventListener('click', onBigUrlImageClick);
+
+const instance = basicLightbox.create(
+  `<img id="image-new" src="" >`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', onEscapeButtonEvent);
+    },
+  },
+  {
+    onClose: instance => {
+      window.removeEventListener('keydown', onEscapeButtonEvent);
+    },
+  }
+);
+
+function onBigUrlImageClick(event) {
+  event.preventDefault();
+
+  console.log(event.target.dataset.source);
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance.element().querySelector('#image-new').src =
+    event.target.dataset.source;
+
+  instance.show();
+}
+
+function onEscapeButtonEvent(event) {
+  event.preventDefault();
+  if (event.code === 'Escape') {
+    instance.close();
+    return;
+  }
+}
+
+// export default {
+//   listOfElements,
+//   listOfImages,
+//   gallery,
+//   instance,
+//   onBigUrlImageClick,
+//   onEscapeButtonEvent,
+// };
