55159155c63e6ad543554e1fb8efa54a3047ba9f
LenaK513 <kryvtsovaolena@gmail.com>
2022-08-27 18:06:17 +0000

    Deploying to gh-pages from @ LenaK513/goit-js-hw-08@3c0b0b4dc99bbaec8845e9120750002083592721 🚀

--- 02-video.ba899760.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(e){console.log("played the video!",e)})),o.getVideoTitle().then((function(e){console.log("title:",e)}))}();
-//# sourceMappingURL=02-video.ba899760.js.map
--- 02-video.f9553c02.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#vimeo-player"),o=new Vimeo.Player(e);o.on("play",(function(e){console.log("played the video!",e)})),o.getVideoTitle().then((function(e){console.log("title:",e)}));
-//# sourceMappingURL=02-video.f9553c02.js.map
