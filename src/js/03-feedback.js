bb60f7edafea960a69519b2d6ad4c47685cf6c42
Olena <kryvtsovaolena@gmail.com>
2022-08-29 21:21:41 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,14 +11,13 @@ form.addEventListener('submit', onFormSumbit);
 onInputDataExist();
 
 function onInputFormEvents(event) {
+  const savedInputData = localStorage.getItem('feedback-form-state');
+  savedInputData = savedInputData ? JSON.parse(savedInputData) : {};
   dataInput[event.target.name] = event.target.value;
 
   localStorage.setItem('feedback-form-state', JSON.stringify(dataInput));
 
-  const savedInputData = localStorage.getItem('feedback-form-state');
-
   const parseInputData = JSON.parse(savedInputData);
-  // console.log(parseInputData);
 }
 
 function onInputDataExist() {
@@ -26,12 +25,9 @@ function onInputDataExist() {
 
   if (getInputData) {
     const parseGetInputData = JSON.parse(getInputData);
-    // console.log(parseGetInputData);
 
     form.email.value = parseGetInputData.email;
     form.message.value = parseGetInputData.message;
-    //   console.log(parseGetInputData.email);
-    //   console.log(parseGetInputData.message);
   }
 }
 
